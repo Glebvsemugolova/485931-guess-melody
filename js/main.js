2667cabd3334932b4a7177f1abb12d84fe815d2f
Gleb Vinokurov <vig@office.sletat.int>
2018-11-14 20:04:22 +0300

    Finished task

---
+++ js/main.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const RIGHT_ARROW = 37;
+const LEFT_ARROW = 39;
+const ARROWS_VISUAL = `
+    <style>
+      .arrows__wrap {
+        position: absolute;
+        top: 135px;
+        left: 50%;
+        margin-left: -56px;
+      }
+      .arrows__btn {
+        background: none;
+        border: 2px solid black;
+        padding: 5px 20px;
+      }
+    </style>
+    <button class="arrows__btn arrows__btn--left"><-</button>
+    <button class="arrows__btn arrows__btn--right">-></button>
+`;
+
+const mainElement = document.querySelector(`section.main`);
+const appElement = document.querySelector(`.app`);
+const screens = Array.from(document.querySelectorAll(`template`));
+
+const selectSlide = (number) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(screens[number].content.cloneNode(true));
+}
+
+let current = 0;
+const select = (index) => {
+  index = index < 0 ? screens.length - 1 : index;
+  index = index >= screens.length ? 0 : index;
+  current = index;
+  selectSlide(current);
+};
+
+document.addEventListener(`keydown`, (evt) => {
+  switch (evt.keyCode) {
+    case RIGHT_ARROW:
+      select(current + 1);
+      break;
+    case LEFT_ARROW:
+      select(current - 1);
+      break;
+  }
+});
+
+select(0);
+
+const arrowBlock = document.createElement("div");
+arrowBlock.innerHTML = arrowsVisual;
+arrowBlock.setAttribute('class', 'arrows__wrap');
+appElement.appendChild(arrowBlock);
+
+const leftARROWS_VISUAL = document.querySelector(`.arrows__btn--left`);
+const rightARROWS_VISUAL = document.querySelector(`.arrows__btn--right`);
+
+document.addEventListener(`click`, (evt) => {
+  switch (evt.target) {
+    case leftARROWS_VISUAL:
+      select(current + 1);
+      break;
+    case rightARROWS_VISUAL:
+      select(current - 1);
+      break;
+  }
+});
